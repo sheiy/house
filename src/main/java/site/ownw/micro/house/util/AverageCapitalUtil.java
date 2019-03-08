9f584a6ef8fa540da1eb84f4c0338f0ae96834be
Sofior <35429674@qq.com>
2019-03-08 18:10:26 +0800

    注册中心

--- src/main/java/site/ownw/micro/house/util/AverageCapitalUtil.java
+++ src/main/java/site/ownw/micro/house/util/AverageCapitalUtil.java
@@ -12,13 +12,13 @@ public abstract class AverageCapitalUtil {
      * 月还款额
      *
      * @param A 贷款总额度
-     * @param β 贷款月利率
+     * @param beta 贷款月利率
      * @param m 总期数
      * @return 月还款额
      */
-    public static BigDecimal monthlyRepayment(BigDecimal A, BigDecimal β, BigDecimal m) {
-        BigDecimal onePlusMonthlyRate = β.add(BigDecimal.ONE).pow(m.intValue());
-        BigDecimal numerator = β.multiply(onePlusMonthlyRate);
+    public static BigDecimal monthlyRepayment(BigDecimal A, BigDecimal beta, BigDecimal m) {
+        BigDecimal onePlusMonthlyRate = beta.add(BigDecimal.ONE).pow(m.intValue());
+        BigDecimal numerator = beta.multiply(onePlusMonthlyRate);
         BigDecimal denominator = onePlusMonthlyRate.subtract(BigDecimal.ONE);
         BigDecimal result = numerator.divide(denominator, 20, RoundingMode.UP);
         return A.multiply(result);
@@ -28,18 +28,18 @@ public abstract class AverageCapitalUtil {
      * 剩余欠款
      *
      * @param A 贷款总额度
-     * @param β 贷款月利率
+     * @param beta 贷款月利率
      * @param n 需要计算还款第几期后剩余的额度
      * @param X 月还款额
      * @return 第n期还款后剩余的欠款
      */
-    public static BigDecimal spareAmount(BigDecimal A, BigDecimal β, BigDecimal n, BigDecimal X) {
+    public static BigDecimal spareAmount(BigDecimal A, BigDecimal beta, BigDecimal n, BigDecimal X) {
         if (n.intValue() <= 0) {
             return A;
         }
-        BigDecimal onePlusMonthlyRate = β.add(BigDecimal.ONE).pow(n.intValue());
+        BigDecimal onePlusMonthlyRate = beta.add(BigDecimal.ONE).pow(n.intValue());
         BigDecimal numerator = X.multiply((onePlusMonthlyRate.subtract(BigDecimal.ONE)));
-        BigDecimal result = A.multiply(onePlusMonthlyRate).subtract(numerator.divide(β, 20, RoundingMode.UP));
+        BigDecimal result = A.multiply(onePlusMonthlyRate).subtract(numerator.divide(beta, 20, RoundingMode.UP));
         if (result.compareTo(BigDecimal.ZERO) < 0) {
             return BigDecimal.ZERO;
         }
